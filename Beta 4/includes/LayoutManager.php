52bf9222366439c3af9c1f898cb7ea34e3060594
Tomas Londoño <tomas.londonom@uqvirtual.edu.co>
2025-12-01 21:35:53 -0500

    Fix LayoutManager (flaticons)

--- Beta 4/includes/LayoutManager.php
+++ Beta 4/includes/LayoutManager.php
@@ -257,6 +257,7 @@ class LayoutManager {
             <link rel="manifest" href="/projectTiendaVirtual/favicon_io/site.webmanifest">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+            <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
             <style><?= $baseCSS . "\n" . $additionalCSS ?></style>
         </head>
         <body>
